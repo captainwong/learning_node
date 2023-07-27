757cb3d90eaf8519000bded662a7df029d00fe72
i7-8700 <1281261856@qq.com>
2023-07-27 23:49:12 +0800

    decorator

---
+++ ts/adv/ch1/class-decorators.ts
@@ -0,0 +1,27 @@
+let classUser = { user: "peter", roles: [{ role: "user" }, { role: "admi1n" }] };
+
+function IsInRoleClass(role: string): boolean{
+    return classUser.roles.some(r => r.role === role);
+}
+
+function RoleClass(role: string) {
+    return function (constructor: Function) {
+        if (!IsInRoleClass(role)) {
+            throw new Error(`The user is not authorized to access this class`);
+        }
+    }
+}
+
+@RoleClass("admin")
+class RestrictedClass{
+    constructor() {
+        console.log(`Inside the constructor`);
+    }
+
+    Validate() {
+        console.log(`Validating`);
+    }
+}
+
+// will throw
+let clazz = new RestrictedClass();
